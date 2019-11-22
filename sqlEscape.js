857b84cb2315417fa5e58edf06de633cc3149494
Justin Haubrich <jhaubrichdesign@gmail.com>
2019-11-22 16:49:28 -0600

    New Solution
    Noticed my previous solution wasn't very robust. Strings like
    `{"test":"t'''es''s't"}` were not working previously. I created a new
    soulution that seems to work better and is way simpler.
    
     Changes to be committed:
    	modified:   sqlEscape.js

--- sqlEscape.js
+++ sqlEscape.js
@@ -1,35 +1,7 @@
-var config = {};
-
+//Need to escape apostraphes in string values of the json object being inserted into the database (' => '')
 function sqlEscapeStr(str) {
 
-	let regex = /:[\s\S]*"[\s\S]*[^'][']{1}[^',]*"/;//matches json object that needs to have an apostraphe escaped
-
-	try { if (typeof str.match(regex)[0] != "undefined") { var replacement= str.match(regex); } }  
-	catch (err) {console.log(err);console.log('sqlEscapeStr error, str and regex:');console.log([str,regex]); var replacement = undefined;}
-	//if no replacement is found, none needs to be made so just return the string unchanged...
-	try { 
-		if (typeof replacement == "undefined") {
-		
-			//set boolean for recursive function to end after all occurences are taken care of
-			config.sqlEscaped = true;
-			return str;
-		} 
-	} catch (err) { 
-		//set boolean for recursive function to end after all occurences are taken care of
-		config.sqlEscaped = true;
-		console.log(err); return str; 
-	}
-
-	//add an extra ' to escape the apostraphe for SQL insertion
-	let escaped = replacement[0].replace(/'/g,"''");
-	let new_str = str.replace(replacement[0], escaped); 
-	
-	//recursively fix all occurences
-	//first check if there are more ' to escape
-	try { if (typeof new_str.match(regex)[0] != "undefined") { window.config.sqlEscaped =false; } }  
-	catch (err) {console.log(err);console.log('sqlEscapeStr error, str and regex:');console.log([str,regex]); config.sqlEscaped = true;}
-	//if the string is not escaped, call function recursively with the new string
-	//return new_str;
-	if (config.sqlEscaped != true) {return sqlEscapeStr(new_str);}
-	else {return new_str;}
+		//get rid of to many apostraphes (never should be more than '')
+		var str = str.split(/'{3,}/g).join("''").split(/(?<!')'(?!')/g).join("''");
+		return str;
 }
